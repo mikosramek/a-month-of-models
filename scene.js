4f9cb029829ad18bb780a931ccdf732a1ff9d1bc
mikosramek <miko@mikosramek.ca>
2025-02-13 15:00:01 -0500

    fix: update OrbitControls import

--- scene.js
+++ scene.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 import modelData from "./models.js";
 
